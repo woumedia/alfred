56e851530f0af58fa9d48006928985252f9e2cc0
Michal Muskala <michal@muskala.eu>
2017-01-31 14:34:54 +0100

    Configure app to use heroku provided port

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 8080))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port') + '!')
 })
