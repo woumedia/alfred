481ba6447408cc242fcfb73918dd27c98b6af114
Michal Muskala <michal@muskala.eu>
2017-01-31 15:35:45 +0100

    Add firebase config for heroku

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var firebase = require('./firebase');
+var app = express();
 
-app.set('port', (process.env.PORT || 8080))
+app.set('port', (process.env.PORT || 8080));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port ' + app.get('port') + '!')
-})
+  console.log('Example app listening on port ' + app.get('port') + '!');
+});
---
+++ app/firebase.js
@@ -0,0 +1,13 @@
+var firebase = require('firebase');
+
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+module.export = firebase;
