02019b28213f95c5ab094e4ce007b454d0393bf5
Michal Muskala <michal@muskala.eu>
2017-02-01 04:45:36 +0100

    Correct wrong event type

--- app/event.js
+++ app/event.js
@@ -1,7 +1,7 @@
 var router = require("./event/router");
 var db = require("./db");
 
-router.subscribe("messages.channel", function(teamId, event) {
+router.subscribe("message.channels", function(teamId, event) {
   db.getConversationChannelId(teamId)
     .then(function(channelId) {
       if (event.channel === channelId) {
