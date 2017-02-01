5bcc200933ea2adc2d67a91d433c86c3d348a70b
Michal Muskala <michal@muskala.eu>
2017-02-01 01:23:04 +0100

    Handle config for slack

--- app/oauth.js
+++ app/oauth.js
@@ -1,12 +1,15 @@
 var slack = require("./slack-client");
 var firebase = require("./firebase-client");
 
+var clientId = process.env.SLACK_CLIENT_ID || "";
+var clientSecret = process.env.SLACK_CLIENT_SECRET || "";
+
 function authenticate(code) {
   if (!code) {
     return Promise.reject("missing code");
   }
 
-  return slack.oauth.access(client_id, client_secret, code)
+  return slack.oauth.access(clientId, clientSecret, code)
     .then(function(resp) {
       console.log(resp);
       return Promise.resolve(resp);
